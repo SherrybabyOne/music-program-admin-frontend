cae6f742ee5b90dbd0fd656eb6e85b91b3b8437c
1185491736@qq.com <1185491736@qq.com>
2019-11-16 12:20:25 +0800

    后台前端页面脚手架搭建

---
+++ src/views/blog/blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    博客
+  </div>
+</template>
+
+<script></script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/playlist/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    歌单
+  </div>
+</template>
+
+<script></script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
